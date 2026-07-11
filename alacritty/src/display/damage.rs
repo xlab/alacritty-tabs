d2e5cfba67cde4415c01e494930d82004db386c0
J M <montytyper@msn.com>
2026-07-11 10:53:33 -0700

    Fix damage y height overlay calculation On odd screen sizes, the damage y height can be offset half a line under where it should, causing half the last line to be visually masked.

--- alacritty/src/display/damage.rs
+++ alacritty/src/display/damage.rs
@@ -226,9 +226,9 @@ impl<'a> RenderDamageIterator<'a> {
     #[inline]
     fn rect_for_line(&self, line_damage: LineDamageBounds) -> Rect {
         let size_info = &self.size_info;
-        let y_top = size_info.height() - size_info.padding_bottom_y();
+        let y_top = size_info.padding_y() + line_damage.line as u32 * size_info.cell_height();
         let x = size_info.padding_x() + line_damage.left as u32 * size_info.cell_width();
-        let y = y_top - (line_damage.line + 1) as u32 * size_info.cell_height();
+        let y = size_info.height() - y_top - size_info.cell_height();
         let width = (line_damage.right - line_damage.left + 1) as u32 * size_info.cell_width();
         Rect::new(x as i32, y as i32, width as i32, size_info.cell_height() as i32)
     }
